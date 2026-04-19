f071be97a5d9908e077c068ae37bac63db06e748
MUGERWAIBRAHIM <mugerwaibrahim90@gmail.com>
2026-04-19 18:05:58 +0300

    updated app.py for render deployment

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from utils import *
 
@@ -27,4 +28,5 @@ def recommend():
     return jsonify({"user_id": user_id, "recommendations": top_movies})
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
